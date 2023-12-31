7c8d100b8e5749a61ecb5d9c94357a18e6b6986a
musuwer <1978445818@qq.com>
2023-12-31 17:41:06 +0800

    recursion a dir(math)

--- func.py
+++ func.py
@@ -1,3 +1,4 @@
+import math
 def func(a,b):
     return a+b
 a,b=map(int,input().split())
@@ -10,3 +11,6 @@ while True:
         break
     else:
         n+=1
+d = dir(math)
+for i in range(0, len(d), 8): #递归地打印列表中的元素，并按每行8个元素的格式进行输出
+    print('\t'.join(d[i:i+8]))
