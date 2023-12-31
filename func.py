bc391e3531a3c12864809ed5eb69eb9a9d036a82
musuwer <1978445818@qq.com>
2023-12-31 11:18:56 +0800

    Adds a Class student

--- func.py
+++ func.py
@@ -1,6 +1,15 @@
+
 def func(a,b):
     return a+b
 a,b=map(int,input().split())
 print(func(a,b))
 for i in range(1,a+b+2):
     print(i)
+class student():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def func2(self):
+        print(f'名字是{self.name}，年龄是{self.age}')
+stu=student('musuwer',18)
+stu.func2()
